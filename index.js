5d9dd6f7cdad49967f27529135de298e2df9e957
ManonVillanueva <g.manon14@gmail.com>
2024-05-28 18:44:32 +0200

    first JS

---
+++ index.js
@@ -0,0 +1,12 @@
+const cursor = document.getElementsByClassName("cursor");
+const mouseMove = document.getElementsByClassName("mouseMove");
+const mouseMoveBefore = document.getElementsByClassName("mouseMoveBefore");
+
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.left = e.pageX + "px";
+//   cursor.style.top = e.pageY + "px";
+//   mouseMove.style.left = e.pageX + "px";
+//   mouseMove.style.top = e.pageY + "px";
+//   mouseMoveBefore.style.left = e.pageX + "px";
+//   mouseMove.style.top = e.pageY + "px";
+// });
